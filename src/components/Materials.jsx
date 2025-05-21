e7a38746a0bd90dadd61349f187921d4542f617d
Ezequiel-Sale <ezequielsale123@gmail.com>
2025-05-21 09:46:43 -0300

    modificacion de archivo app.jsx para solucionar error de importacion

--- src/components/Materials.jsx
+++ src/components/Materials.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import Camera from "./Camera.jsx";
 import { Suspense } from "react";
-import Matcap from "./matcap.jsx";
 import Bricks from "./Bricks.jsx";
 import Light from "./Light";
 import { Environment } from "@react-three/drei";
 import * as THREE from "three";
+import Matcap from "./Matcap.jsx";
 
 const Materials = () => {
   return (
